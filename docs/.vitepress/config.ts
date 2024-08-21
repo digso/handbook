bfeb6ce1aeb5b8eb522ecd540df0ce8e3d006c6c
廿一日的船 <aprosail@outlook.com>
2024-08-21 17:22:18 +0800

    VitePress基本模版和初步配置

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  title: "数社软件开发手册",
+  description: "软件开发基础知识文档",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
