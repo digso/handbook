5f84d1c3a4c269717fddcb21f8fc0bb49c31caec
廿一日的船 <aprosail@outlook.com>
2024-08-23 11:53:17 +0800

    页面自带组件的中文翻译

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ import {wordless} from "./wordless"
 export default defineConfig({
   markdown: {config: (md) => md.use(wordless)},
   base: "/handbook/docs/",
+  lang: "zh-CN",
   title: "数社软件开发手册",
   description: "软件开发基础知识文档",
   themeConfig: {
@@ -12,7 +13,21 @@ export default defineConfig({
       {text: "首页", link: "/"},
       {text: "Flutter", link: "/flutter/flutter"},
     ],
-    sidebar: sidebarMenu,
     socialLinks: [{icon: "github", link: "https://github.com/digso/handbook"}],
+    sidebar: sidebarMenu,
+    outline: {label: "页面大纲", level: [1, 2]},
+    docFooter: {prev: "前页", next: "后页"},
+    editLink: {
+      text: "在 GitHub 上编辑此页面",
+      pattern: "https://github.com/aprosail/notes/edit/dev/:path",
+    },
+    lastUpdated: {
+      text: "最近更新时间",
+      formatOptions: {
+        dateStyle: "medium",
+        timeStyle: "short",
+        forceLocale: true,
+      },
+    },
   },
 })
