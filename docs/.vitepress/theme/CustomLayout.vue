d3f0896fbd9289eb821d1b9597db1b5cdc58e24b
廿一日的船 <aprosail@outlook.com>
2024-08-23 15:46:14 +0800

    限制非必要内容的框选

--- docs/.vitepress/theme/CustomLayout.vue
+++ docs/.vitepress/theme/CustomLayout.vue
@@ -68,31 +68,40 @@
   </Layout>
 </template>
 
-<style scoped lang="scss">
-  h1.title {
-    font-size: 2em;
-    font-weight: 600;
-  }
-</style>
-
 <style lang="scss">
-  div.vp-doc {
-    // 普通段落缩进并两端对齐，使中文排版更美观。
-    p {
-      text-indent: 2rem;
-      text-align: justify;
-      text-justify: inter-word;
+  // 避免对非必要内容的选择。
+  body {
+    user-select: none;
+    div.content-container {
+      user-select: text;
+    }
+  }
+
+  div.content-container {
+    // 自动标题的样式。
+    h1.title {
+      font-size: 2em;
+      font-weight: 600;
     }
 
-    // 除普通段落外的其他段落，如嵌套的内部段落，不添加缩进。
-    div.custom-block p {
-      text-indent: 0;
-    }
+    div.vp-doc {
+      // 普通段落缩进并两端对齐，使中文排版更美观。
+      p {
+        text-indent: 2rem;
+        text-align: justify;
+        text-justify: inter-word;
+      }
 
-    // 取消二级标题上面默认的分割线。
-    h2 {
-      margin-top: 32px;
-      border-top: none;
+      // 除普通段落外的其他段落，如嵌套的内部段落，不添加缩进。
+      div.custom-block p {
+        text-indent: 0;
+      }
+
+      // 取消二级标题上面默认的分割线。
+      h2 {
+        margin-top: 32px;
+        border-top: none;
+      }
     }
   }
 </style>
