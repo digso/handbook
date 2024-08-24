d71eb5525b081ba598c9adc168da6535c36edef2
廿一日的船 <aprosail@outlook.com>
2024-08-24 18:15:18 +0800

    vite-dts临时打包方案(未验证)

--- base/vite.config.ts
+++ base/vite.config.ts
@@ -1,10 +1,16 @@
 import vue from "@vitejs/plugin-vue"
 import {defineConfig} from "vite"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
   build: {
-    lib: {entry: "index.ts", name: "handbook", fileName: "handbook"},
+    lib: {
+      entry: "index.ts",
+      name: "handbook",
+      fileName: "handbook",
+      formats: ["es", "umd"],
+    },
     sourcemap: true,
     outDir: "dist",
     emptyOutDir: true,
